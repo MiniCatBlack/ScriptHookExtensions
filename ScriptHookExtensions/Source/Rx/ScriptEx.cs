6baea6e900f08ece687b1c43095b866bdb8cdc5f
aridai <aridai.net@outlook.jp>
2017-09-28 22:40:11 +0900

    Add PreUpdate event and Update event

---
+++ ScriptHookExtensions/Source/Rx/ScriptEx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GTA.Extensions
+{
+    /// <summary>
+    /// Supports Rx and Coroutine based scripting.
+    /// </summary>
+    public abstract class ScriptEx : Script
+    {
+        /// <summary>
+        /// Occurs before <see cref="Update"/> event occurs.
+        /// </summary>
+        public event EventHandler PreUpdate;
+
+        /// <summary>
+        /// Occurs once per frame.
+        /// </summary>
+        public event EventHandler Update;
+
+        /// <summary>
+        /// Initializes a new instance of the ScriptEx class.
+        /// </summary>
+        protected ScriptEx()
+        {
+
+        }
+
+        private void OnTick(object sender, EventArgs args)
+        {
+            PreUpdate?.Invoke(this, EventArgs.Empty);
+
+            Update?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            Tick -= OnTick;
+        }
+    }
+}
