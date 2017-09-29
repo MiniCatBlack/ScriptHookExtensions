54238e68318a57a25b2bc107ded6554f13d21098
aridai <aridai.net@outlook.jp>
2017-09-29 18:40:46 +0900

    Add ResetAfter

---
+++ ScriptHookExtensions/Source/Rx/ResetAfter.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using UniRx;
+using UniRx.Operators;
+
+namespace GTA.Extensions
+{
+    internal class ResetAfterObservable<T> : OperatorObservableBase<T>
+    {
+        private readonly UniRx.IObservable<T> source;
+
+        private readonly TimeSpan dueTime;
+
+        private readonly IScheduler scheduler;
+
+        private readonly T defaultValue;
+
+        public ResetAfterObservable(UniRx.IObservable<T> source, T defaultValue, TimeSpan dueTime, IScheduler scheduler)
+            : base(scheduler == Scheduler.CurrentThread || source.IsRequiredSubscribeOnCurrentThread())
+        {
+            this.source = source;
+            this.dueTime = dueTime;
+            this.scheduler = scheduler;
+            this.defaultValue = defaultValue;
+        }
+
+        protected override IDisposable SubscribeCore(UniRx.IObserver<T> observer, IDisposable cancel)
+        {
+            return new ResetAfter(this, observer, cancel).Run();
+        }
+
+        internal class ResetAfter : OperatorObserverBase<T, T>
+        {
+            private readonly ResetAfterObservable<T> parent;
+
+            private readonly object gate = new object();
+
+            private SerialDisposable cancellable;
+
+            public ResetAfter(ResetAfterObservable<T> parent, UniRx.IObserver<T> observer, IDisposable cancel) : base(observer, cancel)
+            {
+                this.parent = parent;
+            }
+
+            public IDisposable Run()
+            {
+                cancellable = new SerialDisposable();
+                var subscription = parent.source.Subscribe(this);
+
+                return StableCompositeDisposable.Create(cancellable, subscription);
+            }
+
+            private void OnNext()
+            {
+                lock (gate)
+                {
+                    observer.OnNext(parent.defaultValue);
+                }
+            }
+
+            public override void OnNext(T value)
+            {
+                lock (gate)
+                {
+                    observer.OnNext(value);
+
+                    var d = new SingleAssignmentDisposable();
+                    cancellable.Disposable = d;
+                    d.Disposable = parent.scheduler.Schedule(parent.dueTime, OnNext);
+                }
+            }
+
+            public override void OnError(Exception error)
+            {
+                cancellable.Dispose();
+
+                lock (gate)
+                {
+                    try
+                    {
+                        observer.OnError(error);
+                    }
+
+                    finally
+                    {
+                        Dispose();
+                    }
+                }
+            }
+
+            public override void OnCompleted()
+            {
+                cancellable.Dispose();
+
+                lock (gate)
+                {
+                    try
+                    {
+                        observer.OnCompleted();
+                    }
+
+                    finally
+                    {
+                        Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
