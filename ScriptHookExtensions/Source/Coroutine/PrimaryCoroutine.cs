df78cd86cd15c67eccacd43ca0e0398c63e14f03
aridai <aridai.net@outlook.jp>
2017-09-29 16:36:45 +0900

    Add CoroutineHandle and its inheritances

---
+++ ScriptHookExtensions/Source/Coroutine/PrimaryCoroutine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GTA.Extensions
+{
+    internal class PrimaryCoroutine : CoroutineHandle
+    {
+        public override CoroutineState State { get; internal set; }
+
+        public override void Pause()
+        {
+            if (State == CoroutineState.Running)
+                State = CoroutineState.Pausing;
+        }
+
+        public override void Resume()
+        {
+            if (State == CoroutineState.Pausing)
+                State = CoroutineState.Running;
+        }
+
+        public override void Kill()
+        {
+            State = CoroutineState.Finished;
+        }
+    }
+}
